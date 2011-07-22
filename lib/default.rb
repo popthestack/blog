d4576021bb4f37f61529c4b9e08f4c85192dc34a
Ryan Martinsen <ryan@ryanware.com>
2011-07-21 20:01:46 -0400

    slugging tag urls

--- lib/default.rb
+++ lib/default.rb
@@ -51,8 +51,43 @@ def build_tag_pages
     @items << Nanoc3::Item.new(
       "",
       { :title => "Articles tagged \"#{t}\"", :layout => "tag-page", :tag => t },
-      "/tag/#{t}/"
+      "/tag/#{t.to_slug}/"
     )
   end
 end
 
+# Override link_for_tag functionality (to add .to_slug)
+module Nanoc3::Helpers
+  module Tagging
+    def link_for_tag(tag, base_url)
+      %[<a href="#{h base_url}#{h tag.to_slug}" rel="tag">#{h tag}</a>]
+    end
+  end
+end
+
+# String extensions
+# TODO: use unidecode instead? https://github.com/pjg/ruby_extensions/blob/master/lib/ruby_extensions.rb
+String.class_eval do
+  def to_slug
+    # strip the string
+    ret = self.strip
+
+    # blow away apostrophes
+    ret.gsub! /['`]/,""
+
+    # @ --> at, and & --> and
+    ret.gsub! /\s*@\s*/, " at "
+    ret.gsub! /\s*&\s*/, " and "
+
+    # replace all non alphanumeric, underscore or periods with dash
+    ret.gsub! /\s*[^A-Za-z0-9\.\-]\s*/, '-'
+
+    # convert double underscores to single
+    ret.gsub! /-+/,"-"
+
+    # strip off leading/trailing underscore
+    ret.gsub! /\A[-\.]+|[-\.]+\z/,""
+
+    ret.downcase
+  end
+end
